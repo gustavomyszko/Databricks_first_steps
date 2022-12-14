c5384e9b9cd640327f14b989c7791b3ad7f00944
gustavomyszko@gmail.com <gustavomyszko@gmail.com>
2022-12-14 01:00:33 +0000

    correcao na estrutura da query

--- Dia_01/homework.sql
+++ Dia_01/homework.sql
@@ -1,17 +1,26 @@
 -- Databricks notebook source
 WITH CORE AS(
-SELECT
-  idPedido
-  ,idCliente
-  ,dtEntregue
-  ,datediff(year,dtEntregue,current_date) as qtd_dias_apos_entrega
-FROM silver_olist.pedido
+  SELECT
+    idPedido,
+    idCliente,
+    dtEntregue,
+    datediff(year, dtEntregue, current_date) as qtd_anos_apos_entrega
+  FROM
+    silver_olist.pedido
 ),
 REGRA AS(
-SELECT
-idPedido,
-CASE WHEN qtd_dias_apos_entrega >=5 then 'ready to change' else 'not ready for change' end as status
-FROM CORE
+  SELECT
+    idPedido,
+    CASE
+      WHEN qtd_anos_apos_entrega >= 5 then 'ready to change'
+      else 'not ready for change'
+    end as status
+  FROM
+    CORE
 )
-
-SELECT * except(REGRA.idPedido) FROM CORE LEFT JOIN REGRA ON REGRA.idPedido = CORE.idPedido
+SELECT
+  *
+except(REGRA.idPedido)
+FROM
+  CORE
+  LEFT JOIN REGRA ON REGRA.idPedido = CORE.idPedido
